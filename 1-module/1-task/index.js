aa21000c50e09aae71f2a1b99565dd79364e0007
GostevD <exalted.aims@gmail.com>
2018-06-12 11:24:34 +0300

    done

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,6 +4,13 @@
  * @param {number} n index
  * @returns {number}
  */
-function pow (m, n) {
+function pow(m, n) {
 
+	let result = m;
+
+	for (let i = 1; i < n; i++) {
+		result = result * m;
+	}
+
+	return result;
 }
