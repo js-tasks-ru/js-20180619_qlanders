0557591c2ec9c070088c51a6f2790912ade4a291
Burlak Ilia <burlak@psu.karelia.ru>
2018-07-04 21:34:57 +0300

    add solution for 3-module-2-task

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,9 +1,30 @@
-let calendar = {
-    from: new Date('2018-01-01T15:09:10Z'),
-    to: new Date('2018-02-01T10:09:10Z')
-};
-
 calendar[Symbol.iterator] = function () {
+    let current = this.from;
+    let to = this.to;
+
+    return {
+
+        next() {
+
+            if (current < to) {
+                current.setDate(current.getDate() + 1);
+
+                let value = current.getDate().toString();
+
+                if (value.length === 1) {
+                    value = `0${value}`;
+                }
+
+                if ([0,6].indexOf(current.getDay()) !== -1) {
+                    value = `[${value}]`;
+                }
+
+                return { done: false, value }
+            }
+
+            return { done: true };
+        }
+
+    }
+
 };
-
-
