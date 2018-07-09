91220b991e061397e27a754b7070e394420b1e3b
Dmitriy Gostev <exalted.aims@gmail.com>
2018-07-09 21:06:07 +0300

    4 module done

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -6,4 +6,16 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList (friends) {
+
+    let list = document.createElement('ul');
+
+    for (const friend of friends) {
+        const {firstName, lastName} = friend;
+
+        let item = document.createElement('li');
+        item.innerHTML = `${firstName} ${lastName}`;
+
+        list.appendChild(item);
+    }
+    return list;
 }
